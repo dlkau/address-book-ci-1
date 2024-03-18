770355358270e565497f56cff990b05676ee63e8
test@qut.edu.au <n11251565@qut.edu.au>
2024-03-18 20:48:34 +1000

    (Readings Example 2) - Created a for loop to print out each command line argument.

--- src/W1/Reading_Tasks/Hello_World.java
+++ src/W1/Reading_Tasks/Hello_World.java
@@ -1,2 +1,17 @@
-package W1.Reading_Tasks;public class Hello_World {
+package W1.Reading_Tasks;
+
+/**
+ * In this program, we are returning "Hello World" to the terminal.
+ * A for loop has also been included to print each of the supplied command
+ * line arguments to the terminal.
+ */
+public class Hello_World {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        // Print each of the command line arguments to the terminal
+        for (String arg : args){
+            System.out.println(arg);
+        }
+    }
 }
