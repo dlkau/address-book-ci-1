65fca3a2a2154beb87fa5bc178c5098455b9a6a0
test@qut.edu.au <n11251565@qut.edu.au>
2024-03-18 21:09:52 +1000

    (Readings Example 3) - Added the constructors, getters and setters for the Person object.

--- src/W1/Reading_Tasks/models/Person.java
+++ src/W1/Reading_Tasks/models/Person.java
@@ -1,2 +1,41 @@
-package W1.Reading_Tasks.models;public class Person {
+package W1.Reading_Tasks.models;
+
+public class Person {
+    // The name attribute is private, so it cannot be accessed from outside
+    // the class.
+    private String name;
+
+    /**
+     * A default constructor for the Person class.
+     */
+    public Person(){
+    }
+
+    /**
+     * This is an overloaded constructor for the person class.
+     * @param name the name to be assigned to the Person object.
+     */
+    public Person(String name){
+        setName(name);
+    }
+
+    /**
+     * This is a getter method which returns the name attribute.
+     * @return the name of the attribute.
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * This is a setter for the name attribute which includes validation,
+     * to ensure the name parameter is not empty.
+     * @param name the new name to set the name attribute to.
+     */
+    public void setName(String name){
+        if(name.isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
 }
