144c312bfc30ec55b0037d2c0f2bc55bf0b43dea
test@qut.edu.au <n11251565@qut.edu.au>
2024-03-19 11:14:03 +1000

    (Tutorial Shapes) Implemented the surfaceArea and volume methods from the 'Shape' interface to the 'Cube' class.

--- src/W1/Tutorial_Tasks/src/shapes/Cube.java
+++ src/W1/Tutorial_Tasks/src/shapes/Cube.java
@@ -1,4 +1,36 @@
 package W1.Tutorial_Tasks.src.shapes;
 
-public class Cube implements Shape{
+/**
+ * This Cube class provides an implementation for the
+ * Cube object which inherits from Shape.
+ */
+public class Cube implements Shape {
+    // Define Cube attributes
+    private double edge;
+
+    /**
+     * Define a constructor to instantiate a Cube object.
+     * @param edge The cube edge length.
+     */
+    public Cube(double edge){
+        this.edge = edge;
+    }
+
+    /**
+     * This method calculates the area of the Cube object.
+     * @return the surface area of the cube.
+     */
+    @Override
+    public double surfaceArea(){
+        return 6 * Math.pow(edge, 2);
+    }
+
+    /**
+     * This method calculates the volume of the Cube object.
+     * @return the volume of the cube.
+     */
+    @Override
+    public double volume(){
+        return Math.pow(edge, 3);
+    }
 }
