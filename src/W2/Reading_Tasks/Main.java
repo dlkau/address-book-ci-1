f0964be26730f7b33b84dd319c760a7e307fefea
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-20 08:37:11 +1000

    (Readings) Simulated what happens when Bob made his changes to the file and saves it

--- src/W2/Reading_Tasks/Main.java
+++ src/W2/Reading_Tasks/Main.java
@@ -2,8 +2,7 @@ package W2.Reading_Tasks;
 
 public class Main {
     public static void Main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello, World");
         System.out.println("Welcome to CAB302!");
-        System.out.println("This is our first project!");
     }
 }
