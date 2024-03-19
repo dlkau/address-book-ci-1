173809fab633326a279509e38add50a24e21f675
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-20 08:42:07 +1000

    (Readings) Added welcome message

--- src/W2/Reading_Tasks/src/Main.java
+++ src/W2/Reading_Tasks/src/Main.java
@@ -3,6 +3,6 @@ package W2.Reading_Tasks.src;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Welcome to CAB302!");
     }
-
 }
