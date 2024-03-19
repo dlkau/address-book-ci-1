c77b801e08c2f23787cb433649d58693c2f7cc78
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-20 08:35:24 +1000

    (Readings) Added code simulating what it looked like at the beginning of the day

---
+++ src/W2/Reading_Tasks/Main.java
@@ -0,0 +1,8 @@
+package W2.Reading_Tasks;
+
+public class Main {
+    public static void Main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Welcome to CAB302!");
+    }
+}
