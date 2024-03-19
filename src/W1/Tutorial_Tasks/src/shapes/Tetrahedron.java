fc1ad5cbe5f792cb66b3e19a463853e28a386326
test@qut.edu.au <n11251565@qut.edu.au>
2024-03-19 11:42:29 +1000

    (Tutorial Shapes) Implemented the surfaceArea and volume methods from the 'Shape' interface for the 'Tetrahedron' class

--- src/W1/Tutorial_Tasks/src/shapes/Tetrahedron.java
+++ src/W1/Tutorial_Tasks/src/shapes/Tetrahedron.java
@@ -1,4 +1,34 @@
 package W1.Tutorial_Tasks.src.shapes;
 
+/**
+ * This Tetrahedron class provides an implementation for the
+ * Tetrahedron object which inherits from Shape.
+ */
 public class Tetrahedron implements Shape{
+    // Define Tetrahedron attributes
+    private double edge;
+
+    /**
+     * This constructor instantiates a new tetrahedron object.
+     * @param edge the side length of the tetrahedron object.
+     */
+    public Tetrahedron(double edge){
+        this.edge = edge;
+    }
+
+    /**
+     * Calculates the volume of the tetrahedron.
+     * @return the volume of the given tetrahedron.
+     */
+    public double volume(){
+        return Math.pow(edge,3)/(6.0*Math.sqrt(2));
+    }
+
+    /**
+     * Calculates the surface area of the tetrahedron.
+     * @return the surface area of the given tetrahedron.
+     */
+    public double surfaceArea(){
+        return Math.sqrt(3) * Math.pow(edge, 2);
+    }
 }
