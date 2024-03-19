572f3cf32986f0b4a6742b4d6a6a9863d15033ce
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-20 08:36:29 +1000

    (Readings) Simulated the changes made by Alice

--- src/W2/Reading_Tasks/Main.java
+++ src/W2/Reading_Tasks/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void Main(String[] args){
         System.out.println("Hello World");
         System.out.println("Welcome to CAB302!");
+        System.out.println("This is our first project!");
     }
 }
