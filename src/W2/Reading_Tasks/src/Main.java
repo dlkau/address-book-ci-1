0d5d00fcff004418482ccadd6271b73932b77e14
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-20 09:39:57 +1000

    (Readings) Resolved the conflicts when merging from the W2_WORKING_BRANCH to the W2_print-box branch

--- src/W2/Reading_Tasks/src/Main.java
+++ src/W2/Reading_Tasks/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Welcome to CAB302!");
         System.out.println("This is our first project!");
         System.out.println("Bob likes triangles!");
+        printBox("Bob likes triangles!");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < (4 - i) * 2; j++) {
                 System.out.print(" ");
@@ -16,5 +17,38 @@ public class Main {
             }
             System.out.println();
         }
+        printBox("Alice likes rectangles!");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*****************");
+        }
+    }
+
+    /**
+     * Print a string with a box around it, using the box-drawing characters.
+     * @param str the string to be printed
+     */
+    public static void printBox(String str){
+        int len = str.length() +4;
+        for (int i = 0; i < len; i++){
+            if (i==0){
+                System.out.print("╔");
+            } else if (i == len-1){
+                System.out.print("╗");
+            } else {
+                System.out.print("═");
+            }
+        }
+        System.out.println();
+        System.out.println("║ " + str + " ║");
+        for (int i =0; i < len; i++){
+            if(i==0){
+                System.out.print("╚");
+            } else if (i == len - 1){
+                System.out.print("╝");
+            } else {
+                System.out.print("═");
+            }
+        }
+        System.out.println();
     }
 }
