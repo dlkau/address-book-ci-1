85f8c764be9ef063d64031200f47449b726d68c9
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-20 08:46:00 +1000

    (Readings) Added project message

--- src/W2/Reading_Tasks/src/Main.java
+++ src/W2/Reading_Tasks/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("Welcome to CAB302!");
+        System.out.println("This is our first project!");
     }
 }
