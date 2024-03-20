e41e10fdc78d5e65dfbad65b0e008e2d938c0c53
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-20 13:47:58 +1000

    Added the infiltration files from week 2 to week 3

---
+++ src/W3/Homework_Tasks/Infiltration/obstacles/LocatableObstacle.java
@@ -0,0 +1,39 @@
+package W2.Homework_Tasks.Infiltration.obstacles;
+
+// Add necessary imports
+import W2.Homework_Tasks.Infiltration.common.Location;
+
+/**
+ * Provides the base characteristics of an obstacle that can be located on the map.
+ */
+public abstract class LocatableObstacle implements Obstacle{
+    protected final Location location;
+
+    /**
+     * This constructor instantiates a new LocatableObject based on the supplied location.
+     * @param location the location of the locatable obstacle.
+     */
+    public LocatableObstacle(Location location){
+        this.location = location;
+    }
+
+    /**
+     * This constructor instantiates a new LocatableObject based on the supplied x and y coordinates.
+     * @param x the x coordinate of the LocatableObject.
+     * @param y the y coordinate of the LocatableObject.
+     */
+    public LocatableObstacle(int x, int y){
+        this.location = new Location(x, y);
+    }
+
+    /**
+     * Returns the location of this LocatableObstacle.
+     * @return the location of this LocatableObstacle.
+     */
+    public Location getlocation(){
+        return this.location;
+    }
+
+    public abstract boolean isLocationObstructed(int x, int y);
+    public abstract char getSymbol();
+}
---
+++ src/W3/Homework_Tasks/Infiltration/obstacles/Obstacle.java
@@ -0,0 +1,23 @@
+package W2.Homework_Tasks.Infiltration.obstacles;
+
+/**
+ * This will represent some sort of obstacle that is able to block locations within the map.
+ * The obstacle objects will also be represented by some sort of symbol as well.
+ */
+public interface Obstacle {
+
+    /**
+     * This will return the symbol that is associated with the given obstacle.
+     * @return the symbol of the obstacle.
+     */
+    char getSymbol();
+
+    /**
+     * This method returns a true value if the provide location is obstructed by an obstacle, otherwise
+     * it returns false.
+     * @param x The x coordinate of the location.
+     * @param y The y coordinate of the location.
+     * @return A boolean denoting whether the location has been obstructed by an obstacle or not.
+     */
+    boolean isLocationObstructed(int x, int y);
+}
