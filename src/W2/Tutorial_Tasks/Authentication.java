0d245b5c34bda55294b89513acb09fabda5e3dbb
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-20 11:14:50 +1000

    (Tutorial Authentication) - Added the starting code to the Authentication class

--- src/W2/Tutorial_Tasks/Authentication.java
+++ src/W2/Tutorial_Tasks/Authentication.java
@@ -1,6 +1,16 @@
 package W2.Tutorial_Tasks;
 
+import java.util.ArrayList;
+
 public class Authentication implements IAuthenticationService{
+    private ArrayList<User> users;
+
+    // TODO Now: Add a constructor to initialise the users list with the default user
+
+    // TODO Now: Implement the signUp method to add a new user to the list if the username is not take and return
+    // the user; return null otherwise.
+
+    // TODO Now: Implement the logIn method to return the suer if the username and password match, and null otherwise
 
 
 }
