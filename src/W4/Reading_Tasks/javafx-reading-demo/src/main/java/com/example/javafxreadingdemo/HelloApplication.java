fe02db33acd1452eeff41346e8b86a6cf898423f
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-21 10:36:16 +1000

    (Readings) Updated the HelloApplication start method to load the newly fmxl file instead

--- src/W4/Reading_Tasks/javafx-reading-demo/src/main/java/com/example/javafxreadingdemo/HelloApplication.java
+++ src/W4/Reading_Tasks/javafx-reading-demo/src/main/java/com/example/javafxreadingdemo/HelloApplication.java
@@ -16,33 +16,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        // Define the root node and set the alignment and spacing properties.
-        // Also, set the fillWidth property to false so the children are not
-        // resized to fil the width of the VBox.
-        VBox root = new VBox();
-        root.setAlignment(Pos.CENTER);
-        root.setSpacing(15.0);
-        root.setFillWidth(false);
-        // Create a TextField, a Label and an HBox with appropriate text.
-        TextField textField = new TextField();
-        textField.setText("TextField");
-        Label label = new Label("Label");
-        // The HBox is used to hold the buttons
-        HBox hbox = new HBox();
-        hbox.setAlignment(Pos.CENTER);
-        hbox.setSpacing(15.0);
-        Button button1 = new Button("Button 1");
-        Button button2 = new Button("Button 2");
-        Button button3 = new Button("Button 3");
-        // Add the buttons to the HBox
-        hbox.getChildren().addAll(button1, button2, button3);
-        // Add the children to the root vbox
-        root.getChildren().addAll(textField, label, hbox);
-        // Define the scene, add to the stage (window) and show the stage
-        Scene scene = new Scene(root, 320, 180);
-        stage.setScene(scene);
-        stage.setTitle("JavaFX Example Scene");
-        stage.show();
+       FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+       Scene scene = new Scene(fxmlLoader.load(), 320, 180);
+       stage.setTitle("Hello!");
+       stage.setScene(scene);
+       stage.show();
     }
 
     public static void main(String[] args) {
