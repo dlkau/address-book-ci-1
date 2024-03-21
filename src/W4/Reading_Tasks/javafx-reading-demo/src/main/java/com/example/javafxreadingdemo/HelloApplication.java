f03a0eb336f92777c9180f24e07d30553257c7fe
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-21 10:45:21 +1000

    (Readings) Added additional lines of code in the start method to retrieve the css stylesheet. An empty css stylesheet has also been created

--- src/W4/Reading_Tasks/javafx-reading-demo/src/main/java/com/example/javafxreadingdemo/HelloApplication.java
+++ src/W4/Reading_Tasks/javafx-reading-demo/src/main/java/com/example/javafxreadingdemo/HelloApplication.java
@@ -4,11 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +15,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
        Scene scene = new Scene(fxmlLoader.load(), 320, 180);
+       String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
+       scene.getStylesheets().add(stylesheet);
        stage.setTitle("Hello!");
        stage.setScene(scene);
        stage.show();
