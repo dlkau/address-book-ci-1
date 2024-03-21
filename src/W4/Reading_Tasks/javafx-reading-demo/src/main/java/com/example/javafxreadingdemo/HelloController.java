31516644e2e2a845432407c96a784744641592ba
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-21 11:08:12 +1000

    (Readings) Updated the Controller class to accomodate for button clicks and updated the fxml to include ids and the appropriate controller path

--- src/W4/Reading_Tasks/javafx-reading-demo/src/main/java/com/example/javafxreadingdemo/HelloController.java
+++ src/W4/Reading_Tasks/javafx-reading-demo/src/main/java/com/example/javafxreadingdemo/HelloController.java
@@ -6,9 +6,17 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onButton1Click(){
+        welcomeText.setText("Button 1 was clicked!");
     }
+    @FXML
+    protected void onButton2Click(){
+        welcomeText.setText("Button 2 was clicked!");
+    }
+    @FXML
+    protected void onButton3Click(){
+        welcomeText.setText("Button 3 was clicked!");
+    }
+
 }
