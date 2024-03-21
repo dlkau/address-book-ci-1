30ff0a47c414b4df27b818f06939aafbd074772a
Dylan Kenmuir <n11251565@qut.edu.au>
2024-03-21 11:54:44 +1000

    (Tutorial) Added the checkbox functionality to enable the next button to be selected by the user.

--- src/W4/Tutorial_Tasks/address-book/src/main/java/com/example/addressbook/HelloController.java
+++ src/W4/Tutorial_Tasks/address-book/src/main/java/com/example/addressbook/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.addressbook;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
@@ -9,6 +11,10 @@ public class HelloController {
     private Label welcomeText;
     @FXML
     private TextArea termsAndConditions;
+    @FXML
+    private CheckBox agreeCheckBox;
+    @FXML
+    private Button nextButton;
 
     @FXML
     public void initialize(){
@@ -34,4 +40,11 @@ Elementum nibh tellus molestie nunc non blandit massa.""");
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    protected void onAgreeCheckBoxClick(){
+        boolean accepted = agreeCheckBox.isSelected();
+        nextButton.setDisable(!accepted);
+    }
+
 }
