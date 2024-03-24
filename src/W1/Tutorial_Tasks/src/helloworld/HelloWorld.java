a2fef22a4800a1e6d7fc600c6f60701707b46dee
dlkau <143229795+dlkau@users.noreply.github.com>
2024-03-24 22:10:25 +1000

    (Updated Hello World Excercise Week 1) Added a a statement to return 'Hello World' to the terminal

--- src/W1/Tutorial_Tasks/src/helloworld/HelloWorld.java
+++ src/W1/Tutorial_Tasks/src/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
-package helloworld;
+package W1.Tutorial_Tasks.src.helloworld;
 
 public class HelloWorld {
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
