c18921edcf53ea29d177954cd804443e655af7a2
dlkau <143229795+dlkau@users.noreply.github.com>
2024-04-08 13:58:30 +1000

    (Readings) Added the implementation for the DatabaseConnection object

--- src/W6/Reading_Tasks/DatabaseConnection.java
+++ src/W6/Reading_Tasks/DatabaseConnection.java
@@ -1,4 +1,35 @@
 package W6.Reading_Tasks;
 
+// Add necessary java imports
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Define a singleton class for a database connection.
+ */
 public class DatabaseConnection {
+    /**
+     * The singleton instance of the database connection.
+     */
+    private static Connection instance = null;
+
+    private DatabaseConnection(){
+        String url = "jdbc:sqlite:database.db";
+        try {
+            instance = DriverManager.getConnection(url);
+        } catch (SQLException sqlEx) {
+            System.err.println(sqlEx);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static Connection getInstance() {
+        if (instance == null) {
+            new DatabaseConnection();
+        }
+        return instance;
+    }
 }
