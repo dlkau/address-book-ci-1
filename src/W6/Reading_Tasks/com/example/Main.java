06620f729d517852a0db5e54b1384e21777bb076
dlkau <143229795+dlkau@users.noreply.github.com>
2024-04-09 09:10:43 +1000

    (Reading) Added code to the main method to test the getAll method from the bankAccountDAO class

--- src/W6/Reading_Tasks/com/example/Main.java
+++ src/W6/Reading_Tasks/com/example/Main.java
@@ -2,6 +2,7 @@ package W6.Reading_Tasks.com.example;
 
 // Add necessary imports
 import W6.Reading_Tasks.com.example.DatabaseConnection;
+import java.util.List;
 
 import java.sql.Connection;
 
@@ -13,9 +14,16 @@ public class Main {
         bankAccountDAO.CreateTable();
 
         // Insert some new records
-        bankAccountDAO.insert(new BankAccount("John", "Doe", 10000));
-        bankAccountDAO.insert(new BankAccount("Jane", "Doe", 20000));
-        bankAccountDAO.insert(new BankAccount("Alice", "Smith", 30000));
+        // bankAccountDAO.insert(new BankAccount("John", "Doe", 10000));
+        // bankAccountDAO.insert(new BankAccount("Jane", "Doe", 20000));
+        // bankAccountDAO.insert(new BankAccount("Alice", "Smith", 30000));
+
+        // Retrieve all the records from the bankAccount details
+        List<BankAccount> accounts = bankAccountDAO.getAll();
+        for (BankAccount acc : accounts){
+            System.out.println(acc);
+        }
+
 
         bankAccountDAO.close();
     }
