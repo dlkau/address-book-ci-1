b8dd8db8b1821187d30a5f334be00f0a54d393cf
dlkau <143229795+dlkau@users.noreply.github.com>
2024-04-08 14:16:22 +1000

    (Readings) Added the Main method to the Main class.

--- src/W6/Reading_Tasks/Main.java
+++ src/W6/Reading_Tasks/Main.java
@@ -1,4 +1,11 @@
 package W6.Reading_Tasks;
 
+// Add necessary imports
+import java.sql.Connection;
+
 public class Main {
+    public static void main(String[] args){
+        // Instantiate the connection to the database
+        Connection connection = DatabaseConnection.getInstance();
+    }
 }
