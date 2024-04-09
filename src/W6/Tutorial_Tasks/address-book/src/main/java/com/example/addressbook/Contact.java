afc75c7250b50ea04735450c09b25ec09a9d876c
dlkau <143229795+dlkau@users.noreply.github.com>
2024-04-09 10:11:41 +1000

    (Tutorial) Added the code from the Week 4 Tutorial

---
+++ src/W6/Tutorial_Tasks/address-book/src/main/java/com/example/addressbook/Contact.java
@@ -0,0 +1,68 @@
+package com.example.addressbook;
+
+public class Contact {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    /**
+     * This constructor is used to instantiate a new Contact object.
+     * @param firstname the first name of the contact.
+     * @param lastName The last name of the contact.
+     * @param email The email of the contact.
+     * @param phone The phone number of the contact.
+     */
+    public Contact(String firstname, String lastName, String email, String phone){
+        this.firstName = firstname;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // Below there are a series of getter and setters for the attributes listed above
+    public int getId(){
+        return this.id;
+    }
+
+    public void setID(int id){
+        this.id = id;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public void setFirstname(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPhone(){
+        return this.phone;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public String getFullName(){
+        return firstName + " " + lastName + "(" + email + ")";
+    }
+}
---
+++ src/W6/Tutorial_Tasks/address-book/src/main/java/com/example/addressbook/IContactDAO.java
@@ -0,0 +1,41 @@
+package com.example.addressbook;
+
+import java.util.List;
+
+/**
+ * This interface will be used for the Contact Data Access Object that handles the CRUD operations
+ * for the Contact class with the database.
+ */
+public interface IContactDAO {
+
+    /**
+     * This method will add a new contact to the database.
+     * @param contact the contact to be added to the database.
+     */
+    public void addContact(Contact contact);
+
+    /**
+     * This method will be used to update an existing contact in the database.
+     * @param contact the contact to be updated.
+     */
+    public void updateContact(Contact contact);
+
+    /**
+     * This is the contact which needs to be deleted from the database.
+     * @param contact the contact to be removed from the database.
+     */
+    public void deleteContact(Contact contact);
+
+    /**
+     * This method will retrieve a contact based on the id provided.
+     * @param id the id of the contact to be retrieved from the database.
+     * @return the contact who has the id provided in the method.
+     */
+    public Contact getContact(int id);
+
+    /**
+     * This method returns all the contacts from the database.
+     * @return a list of contacts from the database.
+     */
+    public List<Contact> getAllContacts();
+}
