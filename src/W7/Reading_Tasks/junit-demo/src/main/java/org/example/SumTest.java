cfca78be0b6ff9f4518eedade265b9d37dd2c6b8
dlkau <143229795+dlkau@users.noreply.github.com>
2024-04-16 10:08:55 +1000

    (Readings) Added the implementation for the SumTest class

--- src/W7/Reading_Tasks/junit-demo/src/main/java/org/example/SumTest.java
+++ src/W7/Reading_Tasks/junit-demo/src/main/java/org/example/SumTest.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class SumTest {
+    Sum sum;
+
+    @BeforeEach
+    void setUp(){
+        sum = new Sum();
+    }
+
+    @Test
+    void testOnePlusOne(){
+        int actual = sum.doSum(1,1);
+    }
+
+    @Test
+    void testNegative(){
+        int actual = sum.doSum(-3, -5);
+        int expected = -8;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testLargeNumbers(){
+        assertThrows(Exception.class, () -> sum.doSum(Integer.MAX_VALUE, 1));
+    }
 }
