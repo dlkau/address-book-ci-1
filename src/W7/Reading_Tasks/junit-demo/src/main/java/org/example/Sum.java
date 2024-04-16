b5111256b41650754c2bb48e09da14eea01da106
dlkau <143229795+dlkau@users.noreply.github.com>
2024-04-16 10:13:17 +1000

    (Readings) Added the implementation for the SumTest class

--- src/W7/Reading_Tasks/junit-demo/src/main/java/org/example/Sum.java
+++ src/W7/Reading_Tasks/junit-demo/src/main/java/org/example/Sum.java
@@ -1,4 +1,17 @@
 package org.example;
 
+/**
+ * This sum class is used to sum two numbers together.
+ */
 public class Sum {
+
+    /**
+     * This sum adds two given numbers together.
+     * @param i the first number to be summed.
+     * @param i1 the second number to be summed.
+     * @return the total sum between the two values.
+     */
+    public int doSum(int i, int i1){
+        return i + i1;
+    }
 }
